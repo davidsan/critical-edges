647466d78f7a16ef5ab42d2b8cdd16970a9ed43b
David San <davidsanfr@gmail.com>
2012-10-30 19:07:55 +0100

    Add Reader

---
+++ src/reader.c
@@ -0,0 +1,25 @@
+/**
+ * \file fichier.c
+ * \brief Source de fichier
+ * \author David San
+ */
+#include "reader.h"
+
+FILE *ouvrirFichier(char *filename) {
+	FILE *f = fopen(filename, "r");
+	if (!f) {
+		fprintf(stderr, "ouvrirFichier : erreur d'ouverture du fichier %s\n",
+				filename);
+		return NULL ;
+	}
+	return f;
+}
+
+void fermerFichier(FILE * f) {
+	fclose(f);
+}
+
+int readLine(FILE * f, char *buffer) {
+	return (fgets(buffer, BUFFER_SIZE, f) != NULL );
+}
+
