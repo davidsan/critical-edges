2d3edd8d56ab9f8e71e228d53b628310c19154d2
David San <davidsanfr@gmail.com>
2012-10-30 19:07:51 +0100

    Add Graph

---
+++ src/graph.c
@@ -0,0 +1,202 @@
+/**
+ * \file graph.c
+ * \brief Source de graphe
+ */
+#include "graph.h"
+
+Sommet * creerSommet(int numero, int x, int y) {
+	Sommet * new = (Sommet *) malloc(sizeof(Sommet));
+	if (new == NULL ) {
+		fprintf(stderr, "creerSommet : erreur d'allocation mémoire\n");
+		return NULL ;
+	}
+	new->numero = numero;
+	new->x = x;
+	new->y = y;
+	return new;
+}
+
+Arete * creerArete(Sommet * s1, Sommet * s2, int poids) {
+	Arete * new = (Arete *) malloc(sizeof(Arete));
+	if (new == NULL ) {
+		fprintf(stderr, "creerArete : erreur d'allocation mémoire\n");
+		return NULL ;
+	}
+	new->s1 = s1;
+	new->s2 = s2;
+	new->poids = poids;
+	new->suivant = NULL;
+	return new;
+}
+
+Graphe * creerGraphe(int nbSommets, int nbAretes) {
+	Graphe * new = (Graphe *) malloc(sizeof(Graphe));
+	if (new == NULL ) {
+		fprintf(stderr, "creerGraphe : erreur d'allocation mémoire\n");
+		return NULL ;
+	}
+	new->nbSommets = nbSommets;
+	new->nbAretes = nbAretes;
+
+	new->sommets = (Sommet **) malloc(sizeof(Sommet*) * nbSommets);
+	new->aretes = NULL;
+	return new;
+}
+
+void initGraphe(Graphe * G, int s, int t) {
+	if (G == NULL ) {
+		fprintf(stderr, "initGraphe : graphe invalide\n");
+		return;
+	}
+	G->s = s;
+	G->t = t;
+}
+
+void freeSommet(Sommet * s) {
+	free(s);
+}
+
+void freeArete(Arete * a) {
+	if (a != NULL ) {
+		freeArete(a->suivant);
+	}
+	free(a);
+}
+
+void freeGraphe(Graphe * G) {
+	int i;
+	for (i = 0; i < G->nbSommets; i++) {
+		freeSommet(G->sommets[i]);
+	}
+	free(G->sommets);
+	freeArete(G->aretes);
+	free(G);
+}
+
+Sommet * getSommet(Graphe * G, int n) {
+	if (n < 0 || n >= G->nbSommets) {
+		fprintf(stderr, "getSommet : numéro de sommet invalide\n");
+	}
+	return G->sommets[n];
+}
+
+Arete * ajouterAreteEnTete(Arete * l, Arete * a) {
+	if (a == NULL ) {
+		fprintf(stderr, "ajouterAreteEnTete : arête invalide\n");
+		return NULL ;
+	}
+	a->suivant = l;
+	return a;
+}
+
+Arete * ajouterAreteEnQueue(Arete * l, Arete * a) {
+	Arete * tmp = l;
+	if (l == NULL ) {
+		return a;
+	}
+	while (tmp->suivant != NULL ) {
+		tmp = tmp->suivant;
+	}
+	tmp->suivant = a;
+	return l;
+}
+
+void afficheSommet(Sommet * s) {
+	if (s == NULL ) {
+		fprintf(stderr, "afficheSommet : sommet invalide\n");
+		return;
+	}
+	fprintf(stdout, "%d %d %d\n", s->numero, s->x, s->y);
+}
+
+void afficheArete(Arete * a) {
+	if (a == NULL ) {
+		fprintf(stderr, "afficheArete : arête invalide\n");
+		return;
+	}
+	fprintf(stdout, "%d %d %d\n", a->s1->numero, a->s2->numero, a->poids);
+}
+
+void afficheAretes(Arete * l) {
+	Arete * tmp = l;
+	while (tmp != NULL ) {
+		afficheArete(tmp);
+		tmp = tmp->suivant;
+	}
+}
+
+void afficheGraphe(Graphe * G) {
+	int i;
+	if (G == NULL ) {
+		fprintf(stderr, "afficheGraphe : graphe invalide\n");
+		return;
+	}
+	fprintf(stdout, "%d %d\n", G->nbSommets, G->nbAretes);
+	fprintf(stdout, "%d %d\n", G->s, G->t);
+	for (i = 0; i < G->nbSommets; i++) {
+		afficheSommet(G->sommets[i]);
+	}
+	afficheAretes(G->aretes);
+}
+
+Graphe * lectureGrapheInit(FILE * f) {
+	char buffer[BUFFER_SIZE];
+	char * subStringLeft = NULL;
+	char * subStringRight = NULL;
+	Graphe * new = NULL;
+	if (!readLine(f, buffer)) {
+		fprintf(stderr, "lectureGrapheInit : erreur de lecture du fichier");
+		return NULL ;
+	}
+	subStringLeft = strtok(buffer, " ");
+	subStringRight = strtok(NULL, "\0");
+	int nbSommets = scanInt(subStringLeft);
+	int nbAretes = scanInt(subStringRight);
+
+	if (!readLine(f, buffer)) {
+		fprintf(stderr, "lectureGrapheInit : erreur de lecture du fichier");
+		return NULL ;
+	}
+	subStringLeft = strtok(buffer, " ");
+	subStringRight = strtok(NULL, "\0");
+	int s = scanInt(subStringLeft);
+	int t = scanInt(subStringRight);
+	new = creerGraphe(nbSommets, nbAretes);
+	initGraphe(new, s, t);
+	return new;
+}
+
+Graphe * lectureGraphe(FILE * f) {
+	char buffer[BUFFER_SIZE];
+	char * subStringLeft = NULL;
+	char * subStringCenter = NULL;
+	char * subStringRight = NULL;
+	Graphe * G = NULL;
+	int i = 0;
+	G = lectureGrapheInit(f);
+	for (i = 0; i < G->nbSommets; i++) {
+		if (!readLine(f, buffer)) {
+			fprintf(stderr, "lectureGraphe : erreur de lecture du fichier");
+			return NULL ;
+		}
+		subStringLeft = strtok(buffer, " ");
+		subStringCenter = strtok(NULL, " ");
+		subStringRight = strtok(NULL, "\0");
+		G->sommets[i] = creerSommet(scanInt(subStringLeft),
+				scanInt(subStringCenter), scanInt(subStringRight));
+	}
+	for (i = 0; i < G->nbAretes; i++) {
+		if (!readLine(f, buffer)) {
+			fprintf(stderr, "lectureGraphe : erreur de lecture du fichier");
+			return NULL ;
+		}
+		subStringLeft = strtok(buffer, " ");
+		subStringCenter = strtok(NULL, " ");
+		subStringRight = strtok(NULL, "\0");
+		G->aretes = ajouterAreteEnQueue(G->aretes,
+				creerArete(G->sommets[scanInt(subStringLeft)],
+						G->sommets[scanInt(subStringCenter)],
+						scanInt(subStringRight)));
+	}
+	return G;
+}
---
+++ src/graph.h
@@ -0,0 +1,162 @@
+/**
+ * \file fichier.h
+ * \brief En-tête de graphe
+ */
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "reader.h" /* Pour la constante BUFFER_SIZE */
+#include "scanner.h"
+
+/**
+ * @struct sommet
+ * Structure d'un sommet
+ */
+typedef struct sommet {
+	int numero; /*!< Numéro du sommet */
+	int x; /*!< Coordonnée x du sommet */
+	int y; /*!< Coordonnée y du sommet */
+} Sommet;
+
+/**
+ * @struct arete
+ * Structure d'une arête
+ */
+typedef struct arete {
+	Sommet * s1; /*!< Sommet s1 */
+	Sommet * s2; /*!< Sommet s2 */
+	int poids; /*!< Poids de l'arete (s1, s2) */
+	struct arete * suivant; /*!< Pointeur sur l'arete suivante */
+} Arete;
+
+/**
+ * @struct graphe
+ * Structure d'un graphe
+ */
+typedef struct graphe {
+	int nbSommets; /*!< Nombre de sommets */
+	int nbAretes; /*!< Nombre d'aretes */
+	int s; /*!< Numéro du sommet de départ */
+	int t; /*!< Numéro du sommet d'arrivée */
+	Sommet ** sommets; /*!< Tableau de sommets */
+	Arete * aretes; /*!< Liste chainée d'aretes */
+} Graphe;
+
+/**
+ * @brief Fonction de création d'un sommet
+ * @param x Ordonnée
+ * @param y Abscisse
+ * @param numero Numéro
+ * @return Adresse de la nouvelle instance allouée de type Sommet
+ */
+Sommet * creerSommet(int x, int y, int numero);
+
+/**
+ * @brief Fonction de création d'une arête
+ * @param s1 Premier sommet de l'arête
+ * @param s2 Deuxième sommet de l'arête
+ * @param poids Poids de l'arête
+ * @return Adresse de la nouvelle instance allouée de type Arete
+ */
+Arete * creerArete(Sommet * s1, Sommet * s2, int poids);
+
+/**
+ * @brief Fonction de création d'un graphe
+ * @param nbSommets Nombre de sommets du graphe
+ * @param nbAretes Nombre d'arêtes du graphe
+ * @return Adresse de la nouvelle instance allouée de type Graphe
+ */
+Graphe * creerGraphe(int nbSommets, int nbAretes);
+
+/**
+ * @brief Fonction d'initialisation des sommets s et t du graphe
+ * @param G Graphe à initialiser
+ * @param s Sommet s
+ * @param t Sommet t
+ */
+void initGraphe(Graphe * G, int s, int t);
+
+/**
+ * @brief Fonction de désallocation d'un sommet
+ * @param s Sommet à désallouer
+ */
+void freeSommet(Sommet * s);
+
+/**
+ * @brief Fonction de désallocation d'une arête
+ * @param a Arete à désallouer
+ */
+void freeArete(Arete * a);
+
+/**
+ * @brief Fonction de désallocation d'un graphe
+ * @param G Graphe à désallouer
+ */
+void freeGraphe(Graphe * G);
+
+/**
+ * @brief Fonction de récupération de l'adresse d'un sommet à partir de son numéro
+ * @param G Graphe dans lequel le sommet est situé
+ * @param n Numéro du sommet à récupérer
+ * @return Adresse du sommet recherché
+ */
+Sommet * getSommet(Graphe * G, int n);
+
+/**
+ * @brief Fonction d'ajout en tête de la liste des arêtes
+ * @param l Adresse vers le premier élément de la liste des arêtes
+ * @param a Adresse vers une arête à ajouter
+ * @return Adresse vers le premier élément de la liste une fois l'élément ajouté
+ */
+Arete * ajouterAreteEnTete(Arete * l, Arete * a);
+
+/**
+ * @brief Fonction d'ajout en queue de la liste des arêtes
+ * @param l Adresse vers le premier élément de la liste des arêtes
+ * @param a Adresse vers une arête à ajouter
+ * @return Adresse vers le premier élément de la liste une fois l'élément ajouté
+ */
+Arete * ajouterAreteEnQueue(Arete * l, Arete * a);
+
+/**
+ * @brief Fonction d'affichage d'un sommet
+ * @param s Adresse du sommet à afficher
+ */
+void afficheSommet(Sommet * s);
+
+/**
+ * @brief Fonction d'affichage d'une arête
+ * @param a Adresse de l'arête à afficher
+ */
+void afficheArete(Arete * a);
+
+/**
+ * @brief Fonction d'affichage de la liste des arêtes
+ * @param l Adresse vers le premier élément de la liste des arêtes
+ */
+void afficheAretes(Arete * l);
+
+/**
+ * @brief Fonction d'affichage d'un graphe
+ * @param G Adresse vers le graphe à afficher
+ */
+void afficheGraphe(Graphe * G);
+
+/**
+ * @brief Fonction de lecture d'un graphe à partir d'un flux (initialisation uniquement)
+ * @param f Adresse du flux de lecture
+ * @return Adresse du graphe lu
+ */
+Graphe * lectureGrapheInit(FILE * f);
+
+/**
+ * @brief Fonction de lecture d'un graphe à partir d'un flux
+ * @param f Adresse du flux de lecture
+ * @return Adresse du graphe lu
+ */
+Graphe * lectureGraphe(FILE * f);
+
+#endif
