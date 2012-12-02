9e6f4de9f34d2e250c15a1cc8d8c616a08eab93a
David San <davidsanfr@gmail.com>
2012-12-02 17:52:46 +0100

    Fix output channel

--- src/convertPostscriptCritical.c
+++ src/convertPostscriptCritical.c
@@ -20,13 +20,13 @@ int main(int argc, char **argv) {
 	char *filename = NULL;
 
 	if (argc < 3 || (argc > 1 && (atoi(argv[1]) < 1 || atoi(argv[1]) > 4))) {
-		fprintf(stderr, "usage: %s <datastruct> <gph-files>\n", argv[0]);
-		fprintf(stderr, "\n");
-		fprintf(stderr, "The datastruct parameters are:\n");
-		fprintf(stderr, "   1 : Ordered linked list\n");
-		fprintf(stderr, "   2 : Binary heap\n");
-		fprintf(stderr, "   3 : Packets\n");
-		fprintf(stderr, "   4 : Packets (circular storage)\n");
+		fprintf(stdout, "usage: %s <datastruct> <gph-files>\n", argv[0]);
+		fprintf(stdout, "\n");
+		fprintf(stdout, "The datastruct parameters are:\n");
+		fprintf(stdout, "   1 : Ordered linked list\n");
+		fprintf(stdout, "   2 : Binary heap\n");
+		fprintf(stdout, "   3 : Packets\n");
+		fprintf(stdout, "   4 : Packets (circular storage)\n");
 		return 1;
 	}
 	int i;
@@ -37,30 +37,30 @@ int main(int argc, char **argv) {
 		filename = basename(argv[i]);
 		filename = strtok(filename, ".");
 		filename = strcat(filename, ".ps");
-		fprintf(stderr, "Converting %s to %s ...\n", argv[i], filename);
+		fprintf(stdout, "Converting %s to %s ...\n", argv[i], filename);
 
 		Dijkstra * D = NULL;
 		switch (atoi(argv[1])) {
 		case 2:
-			fprintf(stderr, "Using a binary heap\n");
+			fprintf(stdout, "Using a binary heap\n");
 			D = dijkstraTas(G, G->s);
 			break;
 		case 3:
-			fprintf(stderr, "Using packets\n");
+			fprintf(stdout, "Using packets\n");
 			D = dijkstraPaquet(G, G->s);
 			break;
 		case 4:
-			fprintf(stderr, "Using packets (circular storage)\n");
+			fprintf(stdout, "Using packets (circular storage)\n");
 			D = dijkstraPaquetMod(G, G->s);
 			break;
 		default:
-			fprintf(stderr, "Using an ordered linked list\n");
+			fprintf(stdout, "Using an ordered linked list\n");
 			D = dijkstraListe(G, G->s);
 			break;
 		}
 
 		Arete * sp = extractSP(G, D);
-		fprintf(stderr, "Cost of the shortest path (Dijsktra) : %d\n",
+		fprintf(stdout, "Cost of the shortest path (Dijsktra) : %d\n",
 				calculCout(sp));
 
 		Arete * vitale = NULL;
@@ -88,15 +88,15 @@ int main(int argc, char **argv) {
 		freeArete(vitale);
 		freeArete(sp);
 		freeDijkstra(D);
-		fprintf(stderr, "Done.\n");
-		fprintf(stderr, "\n");
+		fprintf(stdout, "Done.\n");
+		fprintf(stdout, "\n");
 		freeGraphe(G);
 		if (f) {
 			fermerFichier(f);
 		}
 	}
 
-	fprintf(stderr, "All tasks completed.\n");
+	fprintf(stdout, "All tasks completed.\n");
 	free(filename);
 	return 0;
 
